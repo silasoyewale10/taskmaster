d1dc77c792d223255576c657b7a7c7f5ebb24fd6
Silas Oyewale <silasoyewale10@gmail.com>
2020-02-12 01:50:17 -0800

    in progress

---
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -0,0 +1,18 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TaskDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+
+//        Intent word = new Intent();
+//        word.
+    }
+}
